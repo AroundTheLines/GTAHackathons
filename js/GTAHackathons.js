66accfa503353c12907586fff42c89e1c78d3d8f
Qile Wang <wangqile123@gmail.com>
2015-06-04 08:32:37 -0400

    I think it worked!

--- js/GTAHackathons.js
+++ js/GTAHackathons.js
@@ -14,4 +14,13 @@
 		}
 	});
 
+	var hackathons = [{
+		name: "Hack the North",
+		date: "September 18-20, 2015",
+		venue: "University of Waterloo",
+		address:"200 University Ave W, Waterloo, ON N2L 3G1",
+		description: "Hack the North is Canada's premier hackathon. It's an event where 1,000 students with different technical backgrounds and skill levels will come together, for 36 hours, form teams around a problem or idea, and collaboratively code a unique solution from scratch. With world class mentors, food, and hardware resources, you're in for an amazing time!",
+		site: "hackthenorth.com"
+	}];
+
 })(); //end of function wrapper. Good form.
