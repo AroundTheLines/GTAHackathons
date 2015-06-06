41ba5f59e5c5c8afd2489d4cd6cd5149e20edfb6
Qile Wang <wangqile123@gmail.com>
2015-06-05 23:02:12 -0400

    ayy lmao
    pls

--- js/GTAHackathons.js
+++ js/GTAHackathons.js
@@ -3,4 +3,7 @@
 		$(this).toggleClass('current').siblings().removeClass('current');
 	});
 
+	$(".highlight").on("click",function(){
+		alert("Pssst! That's not done yet! Check back in a while ;)");
+	});
 })(); //end of function wrapper. Good form.
