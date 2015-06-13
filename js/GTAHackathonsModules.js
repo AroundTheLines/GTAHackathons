3189c6123b88c5a42fb8a329b1ea2e9fc9d71ec0
Qile Wang <wangqile123@gmail.com>
2015-06-13 16:58:28 -0400

    Hack the north registration opened

--- js/GTAHackathonsModules.js
+++ js/GTAHackathonsModules.js
@@ -74,7 +74,7 @@
 			description: "Hack the North is Canada's premier hackathon. It's an event where 1,000 students with different technical backgrounds and skill levels will come together, for 36 hours, form teams around a problem or idea, and collaboratively code a unique solution from scratch. With world class mentors, food, and hardware resources, you're in for an amazing time!",
 			site: "hackthenorth.com",
 			image: "http://hackthenorth.com/2014/img/logo.png",
-			signups: false
+			signups: true
 		}
 	];
 
